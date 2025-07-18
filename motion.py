9f1ba881846793dc3b40531fe0efdd5e7e5fc320
NR Davis <61124122+NRDavis@users.noreply.github.com>
2025-07-17 23:12:12 -0400

    Add files via upload
    Adding rectangular Drilling patterns

--- motion.py
+++ motion.py
@@ -11,7 +11,7 @@ def endFile(path):
         file.write("M84;\nG02;\n")
     return
 
-def drillBounce(path, depth, plungeD, rise):
+def bounceZ(path, depth, plungeD, rise, moveSpeed):
 	#depth = 10
     #plungeD = 0.5
     #rise=0.1
@@ -21,14 +21,14 @@ def drillBounce(path, depth, plungeD, rise):
         #print(h)
         #file.write("G21;\nG91;\n")
         while h > plungeD:
-            file.write(f"G01 Z-{plungeD} F80;\n")
+            file.write(f"G01 Z-{plungeD} F{moveSpeed};\n")
             file.write(f"G01 Z{rise};\n")
             #print(f"\tG01 Z-{plungeD} F80;\n")
             #print(f"\tG01 Z{rise};\n")
             #print("Hcurrent"+str(h-plungeD+rise))
             h = h - plungeD + rise
         
-        file.write(f"G01 Z-{h} F80;\n")
+        file.write(f"G01 Z-{h} F{moveSpeed};\n")
         #file.write(f"G01 Z{rise};\n")
         #print(f"\tG01 Z-{h} F80;")
         h = 0
@@ -36,4 +36,65 @@ def drillBounce(path, depth, plungeD, rise):
         
         #file.write("M84;\nG02;\n")
         
+    return
+
+def liftZ(path, offset, moveSpeed):
+    with open(path,'a') as file:
+            file.write(f"G01 Z{offset} F{moveSpeed};\n")
+    return
+
+def spindleOn(path, spindleSpeed):
+    with open(path,'a') as file:
+            file.write(f"M03 S{spindleSpeed};\n")
+    return
+
+def spindleOff(path):
+    with open(path,'a') as file:
+            file.write(f"M05;\n")
+    return
+
+#def patternRec(path, rows, cols, spacingX, spacingY, depth, moveSpeed):
+def patternRec(path, startX, startY, endX, endY, depth, rows, cols, moveSpeed, spindleSpeed=None):
+    # move to relative position first
+    xSpacing = (endX-startX)/(rows-1)
+    ySpacing = (endY-startY)/(cols-1)
+    
+    with open(path,'a') as file:
+        # Ensure system is set to relative movements
+        file.write(f"G21;\n")
+        # Turn Spindle On, if not zero
+        if spindleSpeed is not None:
+            file.write(f"M03 S{spindleSpeed};\n")
+        
+        r = 0
+        while r < rows:
+            c = 0
+            
+            while c < (cols):
+                print(f"({r},{c})")
+                if r%2 == 1:
+                    sign = "-"
+                else:
+                    sign = ""
+                # Drill
+                file.write(f"G01 Z-{depth} F{moveSpeed};{r},{c}\n")
+                #file.write(f"G01 Z-{depth} F{moveSpeed};\n")
+                # Lift Z back to original height
+                file.write(f"G01 Z{depth} F{moveSpeed};\n")
+                # Move to next Column
+                c = c + 1
+                if c < cols:
+                    file.write(f"G01 X{sign}{xSpacing} F{moveSpeed};{c-1}\n")
+                    #file.write(f"G01 X{sign}{xSpacing} F{moveSpeed};\n")
+                
+            #advance Y, advance to next row
+            r = r + 1
+            if r < rows:
+                file.write(f"G01 Y{ySpacing} F{moveSpeed};\n")
+            
+            
+        # turn spindle off
+        if spindleSpeed is not None:
+            file.write(f"M05;\n")
+    
     return
